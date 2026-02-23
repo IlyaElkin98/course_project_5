c5b54ce61d4c4f5f4aad057e68c59001016f3a7e
ilya <ilifud98@mail.ru>
2026-02-23 13:38:55 +0700

    refactor:(users) add app users

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+
+class User(AbstractUser):
+    """Модель пользователя"""
+    username = None
+    email = models.EmailField(max_length=100, verbose_name='Электронная почта',
+                              help_text='Введите адрес электронной почты', unique=True)
+    phone = models.CharField(max_length=15, verbose_name='Номер телефона')
+    city = models.CharField(max_length=100, verbose_name='Город проживания')
+
+    objects = UserManager
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["email"]
+
