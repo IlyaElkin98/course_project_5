8a1cb838f42f60aead363edb0859a5913422f774
ilya <ilifud98@mail.ru>
2026-02-26 19:24:40 +0700

    refactor:(habit/users.views) add views.py in app habit and users

--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from habit.models import Habit
+from habit.serializers import HabitSerializer
+
+
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
