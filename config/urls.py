f2e88886eaccec19dda0ec5637c6260e9117c4a0
ilya <ilifud98@mail.ru>
2026-02-26 19:23:57 +0700

    refactor:(users.managers) add managers.py

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('traffic/', include('habit.urls'), name='traffic'),
+    path('api/', include('users.urls'), name='api'),
 ]
