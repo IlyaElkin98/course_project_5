e8d44860ea69f7cf708faaa4f59ff943061de2d4
ilya <ilifud98@mail.ru>
2026-02-27 18:04:12 +0700

    refactor:(habit.paginations): Task:add paginations in project

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,9 @@ STATIC_URL = "static/"
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
 
 AUTH_USER_MODEL = 'users.User'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 
+from habit.paginations import CustomPagination
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -9,3 +10,4 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = CustomPagination
