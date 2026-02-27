198f69200012a27108196d2bac38c3e81e87232b
ilya <ilifud98@mail.ru>
2026-02-27 17:52:50 +0700

    refactor:(habit.validators): Task:add validators in project

--- habit/serializers.py
+++ habit/serializers.py
@@ -1,10 +1,14 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
 from habit.models import Habit
+from habit.validators import HabitValidate
 
 
-class HabitSerializer(serializers.ModelSerializer):
+class HabitSerializer(ModelSerializer):
+    """Сериализатор для привычки"""
 
     class Meta:
         model = Habit
         fields = '__all__'
-
+        ordering_fields = ('id',)
+        validators = [HabitValidate(), ]
--- habit/validators.py
+++ habit/validators.py
@@ -1,29 +1,48 @@
-from django.core.exceptions import ValidationError
-from .models import Habit
+from rest_framework.exceptions import ValidationError
 
 
-def validate_reward_or_related_habit(habit):
-    if habit.reward and habit.related_habit:
-        raise ValidationError("Нельзя одновременно указывать вознаграждение и связанную привычку.")
+class HabitValidate:
+    """Проверка правильности заполнения полей привычки"""
+    """Для сериализатора"""
+    def __init__(
+            self,
+            is_pleasant='is_pleasant',
+            related_habit='related_habit',
+            period='period',
+            reward='reward',
+            duration='duration',
+    ):
+        self.is_pleasant = is_pleasant
+        self.related_habit = related_habit
+        self.period = period
+        self.reward = reward
+        self.duration = duration
 
+    def __call__(self, attrs):
+        # Запрет одновременного выбора связанной привычки и указания вознаграждения
+        if attrs.get(self.related_habit) is not None and attrs.get(self.reward) is not None:
+            message = 'Нельзя выбрать одновременно и связанную привычку и вознаграждение'
+            raise ValidationError(message)
 
-def validate_execution_time(time):
-    if time > 120:
-        raise ValidationError("Время выполнения не должно превышать 120 секунд.")
+        # В связанные привычки могут попадать только привычки с признаком приятной привычки.
+        if attrs.get(self.related_habit) and not attrs[self.related_habit].is_pleasant:
+            message = 'Связанной привычкой можно назначить только приятную привычку'
+            raise ValidationError(message)
 
+        # Время выполнения должно быть не больше 120 секунд.
+        if attrs[self.duration] > 120:
+            message = 'Время выполнения привычки должно быть меньше 2 минут (120 секунд)'
+            raise ValidationError(message)
 
-def validate_pleasant_habit(habit):
-    if habit.is_pleasant:
-        if habit.reward or habit.related_habit:
-            raise ValidationError("У приятной привычки не может быть вознаграждения или связанной привычки.")
+        # У приятной привычки не может быть вознаграждения или связанной привычки.
+        if attrs.get(self.is_pleasant) and (attrs.get(self.reward) or attrs.get(self.related_habit)):
+            message = 'У приятной привычки не может быть вознаграждения или связанной привычки'
+            raise ValidationError(message)
 
-
-def validate_periodicity(period):
-    if period < 1 or period > 7:
-        raise ValidationError("Привычка должна выполняться хотя бы один раз в 7 дней.")
-
-
-def validate_habit(habit):
-    if habit.period > 7:
-        raise ValidationError("Нельзя не выполнять привычку более 7 дней.")
-
+        # Нельзя выполнять привычку реже, чем 1 раз в 7 дней.
+        # Проверка, что поле period передается
+        if attrs.get(self.period):
+            # Проверка значения поля
+            if attrs[self.period] > 7:
+                message = 'Период выполнения привычки должен быть меньше 7 дней'
+                raise ValidationError(message)
